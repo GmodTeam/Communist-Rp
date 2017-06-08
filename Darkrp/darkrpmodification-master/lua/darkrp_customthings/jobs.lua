698ddba2f71b431c26c09e0abe3a811d4cc5f363
GrayN12 <kobeandjameson@gmail.com>
2017-06-08 19:30:49 -0400

    Create jobs.lua

--- Darkrp/darkrpmodification-master/lua/darkrp_customthings/jobs.lua
+++ Darkrp/darkrpmodification-master/lua/darkrp_customthings/jobs.lua
@@ -20,7 +20,7 @@ TEAM_CITIZEN = DarkRP.createJob("Citizen", {
     color = Color(20, 150, 20, 255),
     model = {"models/player/Group01/Female_01.mdl", "models/player/Group01/Female_02.mdl", "models/player/Group01/Female_03.mdl", "models/player/Group01/Female_04.mdl", "models/player/Group01/Female_06.mdl", "models/player/group01/male_01.mdl", "models/player/Group01/Male_02.mdl", "models/player/Group01/male_03.mdl", "models/player/Group01/Male_04.mdl", "models/player/Group01/Male_05.mdl", "models/player/Group01/Male_06.mdl", "models/player/Group01/Male_07.mdl", "models/player/Group01/Male_08.mdl", "models/player/Group01/Male_09.mdl"},
     description = [[Be a simple everyday citizen or be a criminal.]],
-    weapons = {"weapon_fists", },
+    weapons = {"weapon_fists"},
     command = "citizen",
     max = 0,
     salary = 30,
@@ -35,7 +35,7 @@ TEAM_SPETSNAZ = DarkRP.createJob("Spetsnaz", {
     color = Color(119, 21, 21, 255),
     model = {"models/half-dead/modern spetsnaz/male_02.mdl", "models/half-dead/modern spetsnaz/male_01.mdl", "models/half-dead/modern spetsnaz/male_10.mdl"},
     description = [[Special forces of the Red Army.]],
-    weapons = {"tfa_ak74", "tfa_mp9", "arrest_stick", "stunstick", "unarrest_stick", "weaponchecker"},
+    weapons = {"tfa_ak74", "tfa_mp9", "arrest_stick", "stunstick", "unarrest_stick", "weaponchecker", "weapon_fists"},
     command = "spetsnaz",
     max = 6,
     salary = 130,
@@ -51,7 +51,7 @@ TEAM_SOVIETARMY = DarkRP.createJob("Soviet Infantry", {
     color = Color(119, 21, 21, 255),
     model = {"models/blackops/humans/rus/rus_wint_hardline.mdl", "models/blackops/humans/rus/rus_wint_lightweight.mdl"},
     description = [[Infantry of the Red Army.]],
-    weapons = {"tfa_ak74", "arrest_stick", "stunstick", "unarrest_stick", "weaponchecker"},
+    weapons = {"tfa_ak74", "arrest_stick", "stunstick", "unarrest_stick", "weaponchecker", "weapon_fists"},
     command = "sainfantry",
     max = 10,
     salary = 120,
@@ -68,7 +68,7 @@ TEAM_SOVIETARMY = DarkRP.createJob("Soviet Marksman", {
     color = Color(119, 21, 21, 255),
     model = {"models/blackops/humans/rus/rus_wint_ghost.mdl"},
     description = [[Marksman of the Red Army.]],
-    weapons = {"tfa_mp9", "arrest_stick", "stunstick", "unarrest_stick", "weaponchecker", "tfa_intervention"},
+    weapons = {"tfa_mp9", "arrest_stick", "stunstick", "unarrest_stick", "weaponchecker", "tfa_intervention", "weapon_fists"},
     command = "samarksman",
     max = 5,
     salary = 120,
@@ -84,7 +84,7 @@ TEAM_SOVIETARMY = DarkRP.createJob("Soviet Support", {
     color = Color(119, 21, 21, 255),
     model = {"models/blackops/humans/rus/rus_wint_flakjacket.mdl"},
     description = [[Support of the Red Army.]],
-    weapons = {"tfa_mp9", "arrest_stick", "stunstick", "unarrest_stick", "weaponchecker", "tfa_m249lmg"},
+    weapons = {"tfa_mp9", "arrest_stick", "stunstick", "unarrest_stick", "weaponchecker", "tfa_m249lmg", "weapon_fists"},
     command = "sasupport",
     max = 5,
     salary = 120,
@@ -100,7 +100,7 @@ TEAM_SOVIETARMY = DarkRP.createJob("Soviet Officer", {
     color = Color(119, 21, 21, 255),
     model = {"models/blackops/humans/rus/rus_wint_flakjacket.mdl"},
     description = [[Officer of the Red Army.]],
-    weapons = {"tfa_mp9", "arrest_stick", "stunstick", "unarrest_stick", "weaponchecker", "tfa_ak74"},
+    weapons = {"tfa_mp9", "arrest_stick", "stunstick", "unarrest_stick", "weaponchecker", "tfa_ak74", "weapon_fists"},
     command = "saofficer",
     max = 3,
     salary = 140,
@@ -111,6 +111,23 @@ TEAM_SOVIETARMY = DarkRP.createJob("Soviet Officer", {
     category = "Soviet Army",
 	PlayerSpawn = function(ply) ply:SetHealth(150) ply:SetMaxHealth(150) ply:SetArmor(50)  end,
 })
+
+TEAM_SOVIETARMY = DarkRP.createJob("Soviet Governor", {
+    color = Color(119, 21, 21, 255),
+    model = {"models/joshers/badasses/playermodels/odessa.mdl"},
+    description = [[Officer of the Red Army.]],
+    weapons = {"tfa_mp9", "arrest_stick", "unarrest_stick", "weapon_fists"},
+    command = "sogovernor",
+    max = 1,
+    salary = 150,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    category = "Soviet Offical",
+	PlayerSpawn = function(ply) ply:SetHealth(125) ply:SetMaxHealth(125) ply:SetArmor(50)  end,
+})
+
 --[[---------------------------------------------------------------------------
 Define which team joining players spawn into and what team you change to if demoted
 ---------------------------------------------------------------------------]]
